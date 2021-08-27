370de027f0c7cd9a603bc3412cac677b5d3e29bf
dattha <dattha@tutamail.com>
2021-08-27 08:59:49 +0530

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,19 @@
+#  	 __  __       _         __ _ _
+#	|  \/  | __ _| | _____ / _(_) | ___
+#	| |\/| |/ _` | |/ / _ \ |_| | |/ _ \
+#	| |  | | (_| |   <  __/  _| | |  __/
+#	|_|  |_|\__,_|_|\_\___|_| |_|_|\___|
+#
+# source - https://github.com/sdatth/dotfiles
+
+SHELL = /bin/bash
+
+.ONESHELL:
+init:
+	cd terraform
+	terraform init
+	terraform plan
+
+install:
+	cd terraform
+	terraform apply
---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+resource "digitalocean_droplet" "web2" {
+  image  = "ubuntu-20-04-x64"
+  name   = "web2"
+  region = "fra1"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [var.ssh_fingerprint]
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
+
+    connection {
+      host        = self.ipv4_address
+      type        = "ssh"
+      user        = "root"
+      private_key = file(var.pvt_key)
+    }
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      cd ../ansible/
+      ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' --vault-password-file vaultkey add_user.yml
+      ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.username} -i '${self.ipv4_address},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' -e "username=${var.username}" -e ansible_become_pass=${var.upass} site.yml
+    EOT
+  }
+}
+
